388ce1f19217c11085531242799a529337b06ace
Paws <131801287+paws1234@users.noreply.github.com>
2023-11-09 23:16:10 +0800

    Update profile.php
    added security features

--- Group6/profile.php
+++ Group6/profile.php
@@ -91,25 +91,41 @@ $userInfo = $userProfile->getUserInfo();
 <html>
 <head>
     <title>User Profile</title>
-    <link rel="stylesheet" type="text/css" href="css/profile.css">
-    <link rel="icon" href="images/ctu5.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css">
+    <link rel="icon" href="images/ctu.png" type="image/x-icon">
 </head>
-<body>
-    <h1>User Profile</h1>
-    <p>Welcome, <?php echo htmlspecialchars($userInfo['username']); ?>!</p>
-    <h2>Username</h2>
-    <p>Username: <?php echo htmlspecialchars($userInfo['username']); ?></p>
-    <h2>Change Password</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-        <label for="old_password">Old Password:</label>
-        <input type="password" id="old_password" name="old_password" required>
-        <br>
-        <label for="new_password">New Password:</label>
-        <input type="password" id="new_password" name="new_password" required>
-        <br>
-        <input type="submit" name="change_password" value="Change Password">
-    </form>
-
-    <p><a href="user.php">Return to User Dashboard</a></p>
+<body class="bg-gray-100">
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="container mx-auto p-4 sm:p-12 md:p-24 lg:p-32 w-full max-w-screen-lg">
+            <h1 class="text-3xl sm:text-6xl md:text-4xl lg:text-5xl font-semibold text-center mb-4">User Profile</h1>
+            <div class="bg-white p-4 sm:p-6 md:p-8 lg:p-10 rounded-md shadow-md">
+                <div class="text-lg sm:text-3xl font-semibold text-center mb-4">
+                    <p>Welcome, <?php echo htmlspecialchars($userInfo['username']); ?>!</p>
+                </div>
+                <h2 class="text-2xl sm:text-4xl font-semibold mb-4">Username</h2>
+                <div class="mb-4">
+                    <p class="text-gray-700 text-xl sm:text-2xl"><?php echo htmlspecialchars($userInfo['username']); ?></p>
+                </div>
+                <h2 class="text-2xl sm:text-4xl font-semibold mb-4">Change Password</h2>
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                    <div class="mb-4">
+                        <label for="old_password" class="block text-gray-700 mb-2 text-lg sm:text-xl">Old Password:</label>
+                        <input type="password" id="old_password" name="old_password" required class="border border-gray-300 px-3 py-2 rounded-md w-full">
+                    </div>
+                    <div class="mb-4">
+                        <label for="new_password" class="block text-gray-700 mb-2 text-lg sm:text-xl">New Password:</label>
+                        <input type="password" id="new_password" name="new_password" required class="border border-gray-300 px-3 py-2 rounded-md w-full">
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" name="change_password" class="bg-blue-500 text-white text-lg sm:text-xl px-4 py-2 rounded-md hover:bg-blue-600">Change Password</button>
+                    </div>
+                </form>
+            </div>
+            <p class="text-center mt-4">
+                <a href="user.php" class="text-blue-500 hover:underline text-lg sm:text-xl">Return to User Dashboard</a>
+            </p>
+        </div>
+    </div>
 </body>
 </html>
+

19639ae052b176c5d6db217145add5424e2eab0b
Paws <131801287+paws1234@users.noreply.github.com>
2023-11-09 23:17:48 +0800

    Update user.php
    added security features

--- Group6/user.php
+++ Group6/user.php
@@ -31,41 +31,65 @@ class UserDashboard {
         $stmt->execute();
         $userReservations = $stmt->fetchAll(PDO::FETCH_ASSOC);
         ?>
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>User Dashboard</title>
-            <link rel="stylesheet" type="text/css" href="css/user.css">
-            <link rel="icon" href="images/ctu5.png" type="image/x-icon">
-        </head>
-        <body>
-            <h1>User Dashboard</h1>
-            <form method="post" action="">
-                <button type="submit" name="logout">Logout</button>
-                <a href="profile.php"><button class="profile-button" type="button">Profile</button></a>
-            </form>
-            <h2>Your Reservations</h2>
-            <table>
-                <tr>
-                    <th>Date</th>
-                    <th>Computer Count</th>
-                    <th>Purpose</th>
-                    <th>Status</th>
-                </tr>
-                <?php foreach ($userReservations as $reservation): ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>User Dashboard</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+    <link rel="icon" href="images/ctu.png" type="image/x-icon">
+</head>
+
+<body class="bg-gray-100 font-sans p-6 sm:p-12"> 
+    <h1 class="text-6xl sm:text-12xl md:text-18xl font-bold text-center mb-12 sm:mb-24">User Dashboard</h1>
+    <form method="post" action="" class="flex justify-center">
+        <button type="submit" name= "logout"
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-8 sm:py-6 sm:px-10 rounded">Logout</button> 
+        <a href="profile.php"
+            class="bg-green-500 hover-bg-green-700 text-white font-bold py-6 px-8 mx-4 sm:py-6 sm:px-10 rounded">Profile</a> 
+    </form>
+
+    <h2 class="text-6xl sm:text-12xl md:text-12xl font-semibold mt-12 sm:mt-24">Your Reservations</h2> 
+    <div class="overflow-x-auto mt-6 sm:mt-12"> 
+        <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+            <table class="min-w-full">
+                <thead class="bg-blue-500 text-white">
                     <tr>
-                        <td><?= htmlspecialchars($reservation['date']) ?></td>
-                        <td><?= htmlspecialchars($reservation['computer_count']) ?></td>
-                        <td><?= htmlspecialchars($reservation['purpose']) ?></td>
-                        <td><?= htmlspecialchars($reservation['status']) ?></td>
+                        <th class="px-6 py-9 md:w-1/4 lg:w-1/4 xl:w-1/5">Date</th>
+                        <th class="px-6 py-9 md:w-1/4 lg:w-1/4 xl:w-1/5">Computer Count</th> 
+                        <th class="px-6 py-9 md:w-2/4 lg:w-2/4 xl:w-3/5">Purpose</th> 
+                        <th class="px-6 py-9 md:w-1/4 lg:w-1/4 xl:w-1/5">Status</th>
                     </tr>
-                <?php endforeach; ?>
+                </thead>
+                <tbody>
+                    <?php foreach ($userReservations as $reservation): ?>
+                        <tr>
+                            <td class="px-6 py-9"><?= htmlspecialchars($reservation['date']) ?></td> 
+                            <td class="px-6 py-9"><?= htmlspecialchars($reservation['computer_count']) ?></td>
+                            <td class="px-6 py-9"><?= htmlspecialchars($reservation['purpose']) ?></td> 
+                            <td class="px-6 py-9
+                                <?= $reservation['status'] === 'pending' ? 'bg-blue-500' : ($reservation['status'] === 'approved' ? 'bg-green-500' : 'bg-red-500') ?>">
+                                <?= htmlspecialchars($reservation['status']) ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
             </table>
-            <form action="reserve.php">
-                <button type="submit">Create Reservation</button>
-            </form>
-        </body>
-        </html>
+        </div>
+    </div>
+
+    <form action="reserve.php" class="text-center mt-6 sm:mt-12"> 
+        <button type="submit"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-8 sm:py-6 sm:px-10 rounded">Create Reservation</button> 
+    </form>
+</body>
+
+</html>
+
+
+
+
+
         <?php
     }
 }

77a5f3c362679dbf30a8c4e971ffcd11d5f6fd62
Paws <131801287+paws1234@users.noreply.github.com>
2023-11-09 23:14:34 +0800

    Update process_login.php
    added security features

--- Group6/process_login.php
+++ Group6/process_login.php
@@ -21,53 +21,74 @@ class LoginHandler {
         );
     }
 
+    private function validateInput($input) {
+        return htmlspecialchars(trim($input), ENT_QUOTES, 'UTF-8');
+    }
+
+    private function validateUsername($username) {
+      
+        return $this->validateInput($username);
+    }
+
+    private function validatePassword($password) {
+    
+        return $this->validateInput($password);
+    }
+
+    private function redirectTo($role) {
+        $validRoles = ['admin', 'staff', 'user'];
+
+        if (in_array($role, $validRoles, true)) {
+            header("Location: " . htmlspecialchars("$role.php"));
+            exit();
+        } else {
+            $_SESSION['login_error'] = "Invalid role";
+            header("Location: index.php");
+            exit();
+        }
+    }
+
     public function handleLogin() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
+            $username = $this->validateUsername($_POST['username']);
+            $password = $this->validatePassword($_POST['password']);
+
+            if (empty($username) || empty($password)) {
+                $_SESSION['login_error'] = "Invalid input data";
+                header("Location: index.php");
+                exit();
+            }
 
             try {
-                $stmt = $this->db->prepare("SELECT id, password, role FROM users WHERE username = ?");
-                $stmt->execute([$username]);
+                $stmt = $this->db->prepare("SELECT id, password, role FROM users WHERE username = :username");
+                $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+                $stmt->execute();
                 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
                 if ($row) {
                     $hashedPassword = $row['password'];
-
-                    $isValidPassword = true;
-
-                    if ($row['role'] === 'admin' || $row['role'] === 'staff' || $row['role'] === 'user') {
-                        $isValidPassword = sodium_crypto_pwhash_str_verify($hashedPassword, $password);
-                    }
+                    $isValidPassword = sodium_crypto_pwhash_str_verify($hashedPassword, $password);
 
                     if ($isValidPassword) {
                         session_regenerate_id(true);
                         $_SESSION['user_id'] = $row['id'];
                         $_SESSION['user_role'] = $row['role'];
-                    
-                        if ($row['role'] === 'admin') {
-                            header("Location: admin.php");
-                        } elseif ($row['role'] === 'staff') {
-                            header("Location: staff.php");
-                        } elseif ($row['role'] === 'user') {
-                            header("Location: user.php");
-                        } else {
-                            $_SESSION['login_error'] = "Invalid role";
-                            header("Location: login.php");
-                            exit();
-                        }
+                        $this->redirectTo($row['role']);
                     } else {
                         $_SESSION['login_error'] = "Invalid username or password";
-                        header("Location: login.php");
+                        header("Location: index.php");
                         exit();
                     }
                 } else {
                     $_SESSION['login_error'] = "Invalid username or password";
-                    header("Location: login.php");
+                    header("Location: index.php");
                     exit();
                 }
             } catch (PDOException $e) {
-                echo "Database Error: " . $e->getMessage();
+                error_log("Database Error: " . $e->getMessage());
+                $_SESSION['login_error'] = "An error occurred during login. Please try again later.";
+                header("Location: index.php");
+                exit();
             }
         }
     }

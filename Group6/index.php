7e25c972221a2424b48c990f64a77a9df084b9c2
Paws <131801287+paws1234@users.noreply.github.com>
2023-11-09 23:11:54 +0800

    Update index.php
    added client side input validation ,sanitation and htmlescape for xss attacks

--- Group6/index.php
+++ Group6/index.php
@@ -9,13 +9,12 @@
     <script src="https://kit.fontawesome.com/cf223ee5eb.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/index.css">
-    <link rel="icon" href="images/ctu5.png" type="image/x-icon">
+    <link rel="icon" href="images/ctu.png" type="image/x-icon">
     <!-- meta tag that disables caching for the page it is included on and it prevents the browser from storing a local copy. 
     This causes the browser to fetch a fresh copy of the page from the server each time the user visits. -->
     <meta http-equiv="Cache-control" content="no-cache">
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-
 
 </head>
 
@@ -25,34 +24,28 @@
             <div id="slides" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="images/ctu1.jpg"
-                            class="d-block w-100" alt="...">
+                        <img src="images/ctu1.jpg" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="images/ctu2.jpg"
-                            class="d-block w-100" alt="...">
+                        <img src="images/ctu2.jpg" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="images/ctu3.jpg"
-                            class="d-block w-100" alt="...">
+                        <img src="images/ctu3.jpg" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="images/ctu4.jpg"
-                            class="d-block w-100" alt="...">
+                        <img src="images/ctu4.jpg" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="images/ctu6.jpg"
-                            class="d-block w-100" alt="...">
+                        <img src="images/ctu6.jpg" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="images/ctu6.jpg"
-                            class="d-block w-100" alt="...">
+                        <img src="images/ctu6.jpg" class="d-block w-100" alt="...">
                     </div>
                 </div>
             </div>
         </div>
         <div class="text">
-            <h1>Welcome to the<br>Computer Lab <br>Reservation System</S></h1>
+            <h1>Welcome to the<br>Computer Lab <br>Reservation System</h1>
             <div class="bttn">
                 <a class="login-button" data-bs-toggle="modal" data-bs-target="#login-modal"
                     data-bs-whatever="login-modal">
@@ -66,7 +59,7 @@
     </div>
     </div>
     <!------------------------------------ Login Modal ------------------------------------>
-    <div class="modal fade" id="login-modal" tabindex="-1" aria-labelledby="login-modalLabel" aria-hidden="true">
+   <div class="modal fade" id="login-modal" tabindex="-1" aria-labelledby="login-modalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content primary">
                 <div class="modal-header text-center">
@@ -75,22 +68,22 @@
                 </div>
                 <?php
                 if (isset($_SESSION['login_error'])) {
-                    echo '<p class="error-message">' . $_SESSION['login_error'] . '</p>';
+                    echo '<p class="error-message">' . htmlspecialchars($_SESSION['login_error'], ENT_QUOTES, 'UTF-8') . '</p>';
                     unset($_SESSION['login_error']);
                 }
                 ?>
-               <div class="modal-body">
-            <form method="post" action="process_login.php">
-                <div class="mb-3">
-                    <label for="login-username" class="col-form-label">Username:</label>
-                    <input type="text" name="username" class="form-control" id="login-username" required>
-                </div>
-                <div class="mb-3">
-                    <label for="login-password" class="col-form-label">Password:</label>
-                    <input type="password" name="password" class="form-control" id="login-password" required>
-                </div>
+                <div class="modal-body">
+                   <form method="post" action="process_login.php">
+        <div class="mb-3">
+            <label for="login-username" class="col-form-label">Username:</label>
+            <input type="text" name="username" class="form-control" id="login-username" required maxlength="12">
+        </div>
+        <div class="mb-3">
+            <label for="login-password" class="col-form-label">Password:</label>
+            <input type="password" name="password" class="form-control" id="login-password" required maxlength="15">
+        </div>
                         <script>
-                            console.log("User Role: <?php echo isset($_SESSION['user_role']) ? $_SESSION['user_role'] : 'Unknown'; ?>");
+                            console.log("User Role: <?php echo isset($_SESSION['user_role']) ? htmlspecialchars($_SESSION['user_role'], ENT_QUOTES, 'UTF-8') : 'Unknown'; ?>");
                         </script>
                         <div class="d-flex flex-column text-center align-items-center justify-content-center">
                             </button>
@@ -102,9 +95,7 @@
         </div>
     </div>
     <!------------------------------------ Register Modal ------------------------------------>
-    <div class="modal fade" id="reg-modal" tabindex="-1" aria-labelledby="login-modalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered"
-        style="max-width=80%;">
+<div class="modal fade" id="reg-modal" tabindex="-1" aria-labelledby="login-modalLabel" aria-hidden="true">        <div class="modal-dialog modal-dialog-centered" style="max-width=80%;">
             <div class="modal-content primary">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5">Register</h1>
@@ -112,25 +103,24 @@
                 </div>
                 <?php
                 if (isset($_SESSION['registration_error'])) {
-                    echo '<div class="error-message">' . $_SESSION['registration_error'] . '</div>';
+                    echo '<div class="error-message">' . htmlspecialchars($_SESSION['registration_error'], ENT_QUOTES, 'UTF-8') . '</div>';
                     unset($_SESSION['registration_error']);
                 }
                 ?>
                 <div class="modal-body">
                     <form method="post" action="process_registration.php">
-                        <div class="mb-3">
-                            <label for="username" class="col-form-label">Username:</label>
-                            <input type="text" class="form-control" id="username" name="username" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="col-form-label">Password:</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="confirm_password" class="col-form-label">Confirm Password:</label>
-                            <input type="password" class="form-control" id="confirm_password" name="confirm_password"
-                                required>
-                        </div>
+        <div class="mb-3">
+            <label for="username" class="col-form-label">Username:</label>
+            <input type="text" class="form-control" id="username" name="username" required maxlength="12">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="col-form-label">Password:</label>
+            <input type="password" class="form-control" id="password" name="password" required maxlength="15">
+        </div>
+        <div class="mb-3">
+            <label for="confirm_password" class="col-form-label">Confirm Password:</label>
+            <input type="password" class="form-control" id="confirm_password" name="confirm_password" required maxlength="15">
+        </div>
                         <div class="d-flex flex-column text-center align-items-center justify-content-center">
                             </button>
                             <input type="submit" class="login-submit btn btn-primary" value="Register">
@@ -143,3 +133,5 @@
 </body>
 
 </html>
+
+

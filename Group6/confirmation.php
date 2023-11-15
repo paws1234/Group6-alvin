c95c24bc7bd4b9d93f37a9edeeee404c78a16e0a
Paws <131801287+paws1234@users.noreply.github.com>
2023-11-15 13:36:58 +0800

    Update confirmation.php

--- Group6/confirmation.php
+++ Group6/confirmation.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservation Confirmation</title>
-    <link rel="stylesheet" type="text/css" href="css/confirmation.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
     <link rel="icon" href="images/ctu5.png" type="image/x-icon">
 </head>
-<body>
-    <h1>Reservation Confirmation</h1>
-    <p>Your reservation has been successfully confirmed. Thank you for choosing our service!</p>
+<body class="bg-gray-100 font-sans p-4">
+    <div class="container mx-auto text-center p-4">
+        <div class="bg-white rounded-lg p-6 shadow-md">
+            <h1 class="text-3xl md:text-4xl lg:text-5xl font-semibold mb-4 text-blue-500">Reservation Confirmation</h1>
+            <p class="text-lg md:text-xl text-gray-700 mb-4">Your reservation has been successfully confirmed. Thank you for choosing our service!</p>
 
-
-    <p><a href="user.php">Return to User Dashboard</a></p>
+            <p class="text-lg md:text-xl text-blue-500 hover:underline">
+                <a href="user.php">Return to User Dashboard</a>
+            </p>
+        </div>
+    </div>
 </body>
 </html>

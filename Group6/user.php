0c4ee0d2bd3b464d31fe0ada1f0eaa8e017bbd03
Paws <131801287+paws1234@users.noreply.github.com>
2023-11-15 13:42:15 +0800

    Update user.php

--- Group6/user.php
+++ Group6/user.php
@@ -30,67 +30,68 @@ class UserDashboard {
         $stmt->bindParam(':user_id', $userID, PDO::PARAM_INT);
         $stmt->execute();
         $userReservations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
         ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <title>User Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
     <link rel="icon" href="images/ctu.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-<body class="bg-gray-100 font-sans p-6 sm:p-12"> 
-    <h1 class="text-6xl sm:text-12xl md:text-18xl font-bold text-center mb-12 sm:mb-24">User Dashboard</h1>
-    <form method="post" action="" class="flex justify-center">
-        <button type="submit" name= "logout"
-            class="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-8 sm:py-6 sm:px-10 rounded">Logout</button> 
+<body class="bg-gray-100 font-sans p-6 sm:p-12">
+    <h1 class="text-xl sm:text-2xl md:text-4xl lg:text-6xl xl:text-6xl font-bold text-center mb-8 sm:mb-12">User Dashboard</h1>
+    <form method="post" action="" class="flex justify-center space-x-4 mb-8 sm:mb-16">
+        <button type="submit" name="logout"
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 sm:py-6 sm:px-10 rounded">Logout</button>
         <a href="profile.php"
-            class="bg-green-500 hover-bg-green-700 text-white font-bold py-6 px-8 mx-4 sm:py-6 sm:px-10 rounded">Profile</a> 
+            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 sm:py-6 sm:px-10 rounded">Profile</a>
     </form>
 
-    <h2 class="text-6xl sm:text-12xl md:text-12xl font-semibold mt-12 sm:mt-24">Your Reservations</h2> 
-    <div class="overflow-x-auto mt-6 sm:mt-12"> 
+    <h2 class="text-xl sm:text-2xl md:text-4xl lg:text-6xl xl:text-6xl font-semibold mt-8 sm:mt-12 mb-4 sm:mb-6">Your Reservations</h2>
+    <div class="overflow-x-auto mt-6 sm:mt-12">
         <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-            <table class="min-w-full">
-                <thead class="bg-blue-500 text-white">
-                    <tr>
-                        <th class="px-6 py-9 md:w-1/4 lg:w-1/4 xl:w-1/5">Date</th>
-                        <th class="px-6 py-9 md:w-1/4 lg:w-1/4 xl:w-1/5">Computer Count</th> 
-                        <th class="px-6 py-9 md:w-2/4 lg:w-2/4 xl:w-3/5">Purpose</th> 
-                        <th class="px-6 py-9 md:w-1/4 lg:w-1/4 xl:w-1/5">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($userReservations as $reservation): ?>
+            <div class="min-w-full overflow-x-auto">
+                <table class="min-w-full table-auto">
+                    <thead class="bg-blue-500 text-white">
                         <tr>
-                            <td class="px-6 py-9"><?= htmlspecialchars($reservation['date']) ?></td> 
-                            <td class="px-6 py-9"><?= htmlspecialchars($reservation['computer_count']) ?></td>
-                            <td class="px-6 py-9"><?= htmlspecialchars($reservation['purpose']) ?></td> 
-                            <td class="px-6 py-9
-                                <?= $reservation['status'] === 'pending' ? 'bg-blue-500' : ($reservation['status'] === 'approved' ? 'bg-green-500' : 'bg-red-500') ?>">
-                                <?= htmlspecialchars($reservation['status']) ?>
-                            </td>
+                            <th class="px-6 py-3 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-1/5 border">Date</th>
+                            <th class="px-6 py-3 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-1/5 border">Computer Count</th>
+                            <th class="px-6 py-3 sm:w-2/4 md:w-2/4 lg:w-2/4 xl:w-3/5 border">Purpose</th>
+                            <th class="px-6 py-3 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-1/5 border">Status</th>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($userReservations as $reservation): ?>
+                            <tr>
+                                <td class="px-6 py-3 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-1/5 border"><?= htmlspecialchars($reservation['date']) ?></td>
+                                <td class="px-6 py-3 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-1/5 border"><?= htmlspecialchars($reservation['computer_count']) ?></td>
+                                <td class="px-6 py-3 sm:w-2/4 md:w-2/4 lg:w-2/4 xl:w-3/5 border"><?= htmlspecialchars($reservation['purpose']) ?></td>
+                                <td class="px-6 py-3 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-1/5 border
+                                    <?= $reservation['status'] === 'pending' ? 'bg-blue-600': ($reservation['status'] === 'approved' ? 'bg-green-500' : 'bg-red-500') ?>">
+                                    <?= htmlspecialchars($reservation['status']) ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 
-    <form action="reserve.php" class="text-center mt-6 sm:mt-12"> 
+    <form action="reserve.php" class="text-center mt-6 sm:mt-12">
         <button type="submit"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-8 sm:py-6 sm:px-10 rounded">Create Reservation</button> 
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 sm:py-6 sm:px-10 rounded">Create Reservation</button>
     </form>
 </body>
 
 </html>
-
-
-
-
-
-        <?php
+        
+<?php
     }
 }
 
@@ -103,3 +104,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
 $userDashboard = new UserDashboard($conn, $userAuthenticator);
 $userDashboard->userDashboard();
 ?>
+
